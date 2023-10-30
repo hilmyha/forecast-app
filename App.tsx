5e903dc566b619ee7b75846deeab14f31c772de6
Haidar <hilmyahmadhaidar@gmail.com>
2023-10-31 02:44:03 +0700

    setup project

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-100">
+      
+      
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
